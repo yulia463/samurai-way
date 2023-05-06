467762937966470e2bb2ac9a70908ddc56804aef
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-05-06 16:08:32 +0200

    ComponentDidUpdate 74

--- src/components/Profile/ProfileStatus.tsx
+++ src/components/Profile/ProfileStatus.tsx
@@ -37,13 +37,9 @@ export class ProfileStatus extends React.Component<ProfileStatusType> {
     componentDidUpdate(prevProps: any, prevState: any) {
         if (prevProps.status !== this.props.status) {
             this.setState({
-
                 status: this.props.status
-
-
             })
         }
-
     }
 
     render() {
