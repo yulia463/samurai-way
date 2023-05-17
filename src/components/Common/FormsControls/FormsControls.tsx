d16853fcec80bc0f2fad5d4c911e11a5661bb4e4
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-05-17 14:09:07 +0200

    77

---
+++ src/components/Common/FormsControls/FormsControls.tsx
@@ -0,0 +1,5 @@
+export const FormsControls = () => {
+
+    return undefined
+
+}
--- src/utils/validators/Validators.tsx
+++ src/utils/validators/Validators.tsx
@@ -1,7 +1,14 @@
-export const RequiredField = (value: any) => {
+export const Required = (value: any) => {
     if (value) {
         return undefined
     }
-    return 'Field is required  '
+    return 'Field is required'
+
+}
+export const maxLengthCreator = (maxLength: number) => (value: any) => {
+    if (value.length && maxLength > 30) {
+        return `Max length is ${maxLength} symbols`
+    }
+    return undefined
 
 }
