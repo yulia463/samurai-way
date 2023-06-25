fdca58f8a841341ecf8b64581c2a1e2424b6cc18
yuliaslatvitskaya <yuli.slat@yandex.ru>
2023-06-25 20:15:34 +0200

    redirect to profile  78

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Form, Formik, Field} from 'formik';
 import {connect, useDispatch} from "react-redux";
 import {loginTC} from "../../redux/AuthReducer";
+import {Redirect} from "react-router-dom";
 
 type LoginFormProps = {
     onSubmit: (data: any) => void;
@@ -41,12 +42,16 @@ const Login = (props: any) => {
     const dispatch = useDispatch();
     const onSubmit = (formData: any) => {
         dispatch(loginTC(formData.email, formData.password, formData.rememberMe))
-
+    }
+    if(props.isAuth){
+        return <Redirect to={'/profile'}/>
     }
     return <div>
         <h1>LOGIN</h1>
         <LoginForm onSubmit={onSubmit}/>
-        {/*<LoginReduxForm onSubmit={onSubmit}/>*/}
     </div>
 }
-export default connect(null, {loginTC})(Login)
+const mapStateToProps =(state:any)=>({
+    isAuth:state.auth.isAuth
+})
+export default connect(mapStateToProps, {loginTC})(Login)
